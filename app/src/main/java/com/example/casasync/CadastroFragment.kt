aa04283351bd397573b577fb9d3ece1b75e15edf
Carlos <csnj@outlook.com>
2025-09-20 22:57:47 -0300

    lógica do login criada para testes (em memória)

--- app/src/main/java/com/example/casasync/CadastroFragment.kt
+++ app/src/main/java/com/example/casasync/CadastroFragment.kt
@@ -6,9 +6,19 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import android.widget.TextView
+import android.widget.Toast
 
 class CadastroFragment : Fragment() {
 
+    // banco de dados (em memória)
+    companion object {
+        var users = mutableListOf<User>()
+    }
+
+    // classe que representa um usuário (em memória)
+    data class User(val name: String, val login: String, val password: String)
+
+    // função para inflar o layout do fragment_cadastro.xml
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +30,27 @@ class CadastroFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // lógica para guardar os cadastros (em memória)
+        val newUserPrompt = view.findViewById<TextView>(R.id.newUserPrompt)
+        val newLoginPrompt = view.findViewById<TextView>(R.id.newLoginPrompt)
+        val newPasswordPrompt = view.findViewById<TextView>(R.id.newPasswordPrompt)
+
+        val btnCadastro = view.findViewById<TextView>(R.id.btnCadastro)
+
+        btnCadastro.setOnClickListener {
+            val name = newUserPrompt.text.toString()
+            val login = newLoginPrompt.text.toString()
+            val password = newPasswordPrompt.text.toString()
+
+            if (name.isNotEmpty() && login.isNotEmpty() && password.isNotEmpty()) {
+                users.add(User(name, login, password))
+                Toast.makeText(requireContext(), getString(R.string.cadastro_success_message), Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), getString(R.string.cadastro_error_message), Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // lógica da troca de tela para o login
         val btnLoginAccount = view.findViewById<TextView>(R.id.btnLoginAccount)
 
         btnLoginAccount.setOnClickListener {
--- app/src/main/java/com/example/casasync/LoginFragment.kt
+++ app/src/main/java/com/example/casasync/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import android.widget.TextView
+import android.widget.Toast
 
 class LoginFragment : Fragment() {
 
@@ -19,6 +20,36 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val txtLoginPrompt = view.findViewById<TextView>(R.id.txtLoginPrompt)
+        val txtPasswordPrompt = view.findViewById<TextView>(R.id.txtPasswordPrompt)
+
+        val btnLogin = view.findViewById<TextView>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val login = txtLoginPrompt.text.toString()
+            val password = txtPasswordPrompt.text.toString()
+
+            if (login.isNotEmpty() && password.isNotEmpty()) {
+                val userFound = CadastroFragment.users.find {
+                    it.login == login && it.password == password
+                }
+
+                if (userFound != null) {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.login_success_message),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.login_error_message),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
 
         val btnCreateAccount = view.findViewById<TextView>(R.id.btnCreatAccount)
 
