49cd8d2214ab08bc2e7b98d5de584a0181fbb9de
Anthony Rychard <anthonyrychard07@gmail.com>
2025-09-26 20:23:18 +0000

    mudanças em cadastro

--- app/src/main/java/com/example/casasync/CadastroFragment.kt
+++ app/src/main/java/com/example/casasync/CadastroFragment.kt
@@ -45,6 +45,18 @@ class CadastroFragment : BaseFragment(R.layout.fragment_cadastro) {
             if (name.isNotEmpty() && login.isNotEmpty() && password.isNotEmpty()) {
                 users.add(User(name, login, password))
                 Toast.makeText(requireContext(), getString(R.string.cadastro_success_message), Toast.LENGTH_SHORT).show()
+
+                parentFragmentManager.beginTransaction()
+                .setCustomAnimations(
+                    R.anim.slide_in_left,
+                    R.anim.slide_out_right,
+                    R.anim.slide_in_right,
+                    R.anim.slide_out_left
+                )
+                .replace(R.id.fragment_container, LoginFragment())
+                .addToBackStack(null)
+                .commit()
+
             } else {
                 Toast.makeText(requireContext(), getString(R.string.cadastro_error_message), Toast.LENGTH_SHORT).show()
             }
