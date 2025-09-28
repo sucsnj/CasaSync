78da3516b1998b1b30b50baee4edc8d544767d35
Carlos <csnj@outlook.com>
2025-09-27 21:59:19 -0300

    adição de comentários ao SnackbarUtils e MyApp

--- app/src/main/java/com/example/casasync/MyApp.kt
+++ app/src/main/java/com/example/casasync/MyApp.kt
@@ -3,12 +3,14 @@ package com.example.casasync
 import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 
+// classe de configuração inicial do app
+// essa classe carrega antes da aplicação em si, a main activity
 class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
 
-        // força o modo claro quando o app inicia
+        // força o modo claro
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
