9aaf7ca91312394d453ec56378e6dc60655d23bc
Carlos <csnj@outlook.com>
2025-10-02 17:08:21 -0300

    login com firestore

--- app/src/main/java/com/devminds/casasync/LoginFragment.kt
+++ app/src/main/java/com/devminds/casasync/LoginFragment.kt
@@ -4,91 +4,71 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import com.devminds.casasync.Utils.safeShowDialog
+import com.google.firebase.firestore.FirebaseFirestore
 
 class LoginFragment : BaseFragment(R.layout.fragment_login) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // testando o firestore
-        FirestoreHelper.escreverUsuario()
-        FirestoreHelper.lerUsuarios()
-
-        // guarda os dados de login e senha
         val txtLoginPrompt = view.findViewById<TextView>(R.id.txtLoginPrompt)
-        val txtPasswordPrompt = view.findViewById<TextView>(R.id.txtPasswordPrompt)
 
-        // representa o botão de login
         val btnLogin = view.findViewById<TextView>(R.id.btnLogin)
-
-        // o que fazer ao clicar no botão de login
         btnLogin.setOnClickListener {
 
-            // transforma os dados em string
-            val login = txtLoginPrompt.text.toString()
-            val password = txtPasswordPrompt.text.toString()
+            val login = txtLoginPrompt.text.toString().trim()
 
-            // se estiver tudo preenchido
-            if (login.isNotEmpty() && password.isNotEmpty()) {
+            if (login.isNotEmpty()) {
 
-                // pega o usuário com os dados de login e senha
-                val userFound = CadastroFragment.users.find {
-                    it.login == login && it.password == password
-                }
+                val db = FirebaseFirestore.getInstance()
+                val usersCollection = db.collection("usuarios")
 
-                // se o usuário for encontrado
-                if (userFound != null) {
+                usersCollection.whereEqualTo("nome", login).get()
+                    .addOnSuccessListener { result ->
+                        if (!result.isEmpty) {
+                            safeShowDialog(getString(R.string.login_found_message))
 
-                    // mostra a mensagem de sucesso e troca de tela
-                    context?.let {
-                        safeShowDialog(getString(R.string.login_success_message))
+                            parentFragmentManager.beginTransaction()
+                                .setCustomTransition(TransitionType.FADE)
+                                .replace(R.id.fragment_container, HomeFragment())
+                                .addToBackStack(null)
+                                .commit()
+                        } else {
+                            safeShowDialog(getString(R.string.login_error_message))
+                        }
                     }
-
-                    // troca de tela para a tela inicial
-                    parentFragmentManager.beginTransaction()
-                        .setCustomTransition(TransitionType.SLIDE)
-
-                        // muda o fragmento (tela)
-                        .replace(R.id.fragment_container, HomeFragment())
-                        .addToBackStack(null) // guarda a tela atual para voltar quando precisar
-                        .commit() // finaliza a transação
-                } else {
-                    // mostra a mensagem de erro
-                    safeShowDialog(getString(R.string.login_error_message))
-                }
+                    .addOnFailureListener { e ->
+                        safeShowDialog(getString(R.string.login_error_message)) // TODO strings
+                    }
             } else {
-                safeShowDialog(
-                    getString(R.string.login_empty_message)
-                )
+                safeShowDialog(getString(R.string.login_empty_message))
             }
-        }
 
-        // representa o botão de criar conta
-        val btnCreateAccount = view.findViewById<TextView>(R.id.btnCreatAccount)
 
-        // o que fazer ao clicar no botão de criar conta
-        btnCreateAccount.setOnClickListener {
+            val btnCreateAccount = view.findViewById<TextView>(R.id.btnCreatAccount)
+            btnCreateAccount.setOnClickListener {
 
-            // troca de tela para o cadastro
-            parentFragmentManager.beginTransaction()
-                .setCustomTransition(TransitionType.SLIDE)
+                // troca de tela para o cadastro
+                parentFragmentManager.beginTransaction()
+                    .setCustomTransition(TransitionType.SLIDE)
 
-                // muda o fragmento (tela)
-                .replace(R.id.fragment_container, CadastroFragment())
-                .addToBackStack(null) // guarda a tela atual para voltar quando precisar
-                .commit() // finaliza a transação
-        }
+                    // muda o fragmento (tela)
+                    .replace(R.id.fragment_container, CadastroFragment())
+                    .addToBackStack(null) // guarda a tela atual para voltar quando precisar
+                    .commit() // finaliza a transação
+            }
 
-        // representa o botão de recuperar senha
-        val btnForgotPassword = view.findViewById<TextView>(R.id.txtForgotPassword)
+            // representa o botão de recuperar senha
+            val btnForgotPassword = view.findViewById<TextView>(R.id.txtForgotPassword)
 
-        // o que fazer ao clicar no botão de recuperar senha
-        btnForgotPassword.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .setCustomTransition(TransitionType.SLIDE) // animação de troca de tela
-                .replace(R.id.fragment_container, RecoveryFragment())
-                .addToBackStack(null) // guarda a tela atual para voltar quando precisar
-                .commit() // finaliza a transação
+            // o que fazer ao clicar no botão de recuperar senha
+            btnForgotPassword.setOnClickListener {
+                parentFragmentManager.beginTransaction()
+                    .setCustomTransition(TransitionType.SLIDE) // animação de troca de tela
+                    .replace(R.id.fragment_container, RecoveryFragment())
+                    .addToBackStack(null) // guarda a tela atual para voltar quando precisar
+                    .commit() // finaliza a transação
+            }
         }
     }
 }
