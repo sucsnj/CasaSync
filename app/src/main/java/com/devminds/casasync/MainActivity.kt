391c7789f8f5d3c24d33b6f5fc8bca7435ab42bf
Carlos <csnj@outlook.com>
2025-10-07 21:38:51 -0300

    inserção do menu lateral

--- app/src/main/java/com/devminds/casasync/MainActivity.kt
+++ app/src/main/java/com/devminds/casasync/MainActivity.kt
@@ -5,9 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.devminds.casasync.R
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +24,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // menu lateral
+//        drawerLayout = findViewById(R.id.drawer_layout)
+//        navView = findViewById(R.id.nav_view)
+//
+//        navView.setNavigationItemSelectedListener { menuItem ->
+//            when (menuItem.itemId) {
+//                // R.id.nav_home -> replaceFragment(HomeFragment())
+//                // R.id.nav_settings_app -> replaceFragment(SettingsAppFragment())
+//                // R.id.nav_settings_user -> replaceFragment(SettingsUserFragment())
+//                R.id.nav_about -> replaceFragment(AboutFragment())
+//            }
+//            drawerLayout.closeDrawer(GravityCompat.START)
+//            true
+//        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .setCustomTransition(TransitionType.INFLATE)
+            .replace(R.id.fragment_container, fragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
