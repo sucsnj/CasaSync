7bbe125b53a572ff7d4b40719b98275db4422db6
Carlos <csnj@outlook.com>
2025-10-28 19:38:11 -0300

    cor do entalhe

--- app/src/main/java/com/devminds/casasync/MainActivity.kt
+++ app/src/main/java/com/devminds/casasync/MainActivity.kt
@@ -8,14 +8,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    // private lateinit var drawerLayout: DrawerLayout
-    // private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,21 +30,6 @@ class MainActivity : AppCompatActivity() {
                 1001 // codigo qualquer
             )
         }
-
-        // menu lateral
-//        drawerLayout = findViewById(R.id.drawer_layout)
-//        navView = findViewById(R.id.nav_view)
-//
-//        navView.setNavigationItemSelectedListener { menuItem ->
-//            when (menuItem.itemId) {
-//                // R.id.nav_home -> replaceFragment(HomeFragment())
-//                // R.id.nav_settings_app -> replaceFragment(SettingsAppFragment())
-//                // R.id.nav_settings_user -> replaceFragment(SettingsUserFragment())
-//                R.id.nav_about -> replaceFragment(AboutFragment())
-//            }
-//            drawerLayout.closeDrawer(GravityCompat.START)
-//            true
-//        }
     }
 
     private fun replaceFragment(fragment: Fragment, transitionType: TransitionType) {
