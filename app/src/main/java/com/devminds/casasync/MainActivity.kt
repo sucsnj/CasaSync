25af2785b7b4b6193ef0f8b8a750b77ad094fc34
Carlos <csnj@outlook.com>
2025-11-01 15:36:04 -0300

    melhorias no sistema de permissões

--- app/src/main/java/com/devminds/casasync/MainActivity.kt
+++ app/src/main/java/com/devminds/casasync/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.devminds.casasync
 
 import android.Manifest
+import android.app.AlarmManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import androidx.core.net.toUri
+import com.devminds.casasync.utils.DialogUtils
+import com.devminds.casasync.utils.PermissionHelper
+import com.devminds.casasync.utils.PermissionHelper.checkAndRequestExactAlarmPermission
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,13 +34,19 @@ class MainActivity : AppCompatActivity() {
         }
 
         // pedido de permissão para notificações
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) { // Android 13
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
-                1001 // codigo qualquer
-            )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            PermissionHelper.checkAndRequestPostNotificationPermission(this)
         }
+    }
+
+    // verifica se a permissão de notificação foi dada
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        PermissionHelper.handlePermissionResult(this, requestCode, permissions, grantResults)
     }
 
     private fun replaceFragment(fragment: Fragment, transitionType: TransitionType) {
