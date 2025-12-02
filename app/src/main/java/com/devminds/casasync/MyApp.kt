2112f7367d3d661de6e115307b744f6367428487
sucsnj <78510176+sucsnj@users.noreply.github.com>
2025-12-02 15:31:00 +0000

    cores do tema

--- app/src/main/java/com/devminds/casasync/MyApp.kt
+++ app/src/main/java/com/devminds/casasync/MyApp.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatDelegate
 import com.devminds.casasync.utils.NotificationUtils
 import com.google.firebase.FirebaseApp
+import androidx.preference.PreferenceManager
 
 // classe de configuração inicial do app
 // essa classe carrega antes da aplicação em si, a main activity
@@ -12,8 +13,17 @@ class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        // forçar o modo claro
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        // define o tema do app com base na preferência salva
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        val isDarkMode = prefs.getBoolean("dark_mode", false)
+
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        // inicializa o Firebase
         FirebaseApp.initializeApp(this)
 
         // Cria o canal de notificação ao iniciar o app
